94624b83ec75f10f9bbc4895df50fd6332b8d991
William Fosmark Haugland <31375260+William58141@users.noreply.github.com>
2022-06-05 20:50:57 +0200

    Update db.php

--- db.php
+++ db.php
@@ -3,8 +3,8 @@ class mysqlPDO extends PDO {
   public function __construct() {
     $drv = 'mysql';
     $hst = 's381.usn.no';
-    $usr = 'usr_valg';
-    $pwd = 'pw_valg2021';
+    $usr = 'root';
+    $pwd = 'root';
     $sch = 'valg2021';
     $dsn = $drv . ':host=' . $hst . ';dbname=' . $sch;
   parent::__construct($dsn,$usr,$pwd);
